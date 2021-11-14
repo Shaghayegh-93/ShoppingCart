c3153b8a9f457a4b20ea10c6318276969408c38e
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-11-14 13:52:46 +0330

    add pages folder

---
+++ src/Pages/CartPage.js
@@ -0,0 +1,12 @@
+import Layout from "../Layout/Layout";
+import Cart from "../components/Cart/Cart";
+
+const CartPage = () => {
+    return ( 
+        <Layout>
+            <Cart/>
+        </Layout>
+     );
+}
+ 
+export default CartPage;
