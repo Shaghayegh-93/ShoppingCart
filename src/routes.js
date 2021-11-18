685bdd9a5979bf34a3361c081f43bbe6e5bdbb8d
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-11-18 13:54:22 +0330

    add new root for develop the project

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
-import HomePage from "./Pages/HomePage"
+import HomePage from "./Pages/HomePage";
 import CartPage from "./Pages/CartPage";
-const routes=[
-    {path:"/" , component:HomePage ,exact:true},
-    {path:"/cart" , component:CartPage},
-]
+import ChekoutPage from "./Pages/ChekoutPage";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
+const routes = [
+  { path: "/cart", component: CartPage },
+  { path: "/checkout", component: ChekoutPage },
+  { path: "./login", component: LoginPage },
+  { path: "./signup", component: SignupPage },
+  { path: "/", component: HomePage, exact: true },
+];
 
 export default routes;
