061019320162a8d79f77691689b7232edc7d20f5
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-11-18 13:52:54 +0330

    develop the HomePage component

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,43 @@
 import Layout from "../Layout/Layout";
+import * as data from "../data";
+import { useCart, useCartActions } from "../Context/CartProvider";
+import checkInCart from "../utils/checkInCart";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
+
 
 const HomePage = () => {
+  const { cart } = useCart();
+  const dispatch = useCartActions();
+  const addProductHandler = (product) => {
+    dispatch({ type: "ADD_TO_CART", payload: product });
+    toast.success(`${product.name} added to cart!`);
+  };
   return (
     <Layout>
-      <h2>this is home page</h2>
+      <main className="container">
+        <section className="productList">
+          {data.products.map((product) => {
+            return (
+              <section className="product" key={product.id}>
+                <div className="productImg">
+                  <img src={product.image} alt={product.name}></img>
+                </div>
+                <div className="productDesc">
+                  <p>{product.name}</p>
+                  <p>${product.price}</p>
+                  <button
+                    onClick={() => addProductHandler(product)}
+                    className="btn primary"
+                  >
+                    {checkInCart(cart, product) ? <Link to="/cart" className="btn primary">In Cart</Link> : " Add to Cart"}
+                  </button>
+                </div>
+              </section>
+            );
+          })}
+        </section>
+      </main>
     </Layout>
   );
 };
