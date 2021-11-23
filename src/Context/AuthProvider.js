02c92931747aafaeeb8a6e63bc27c70450451008
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-11-23 13:14:40 +0330

    make new component for authorization users

---
+++ src/Context/AuthProvider.js
@@ -0,0 +1,31 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+const AuthContextDispatcher = createContext();
+const LOCAL_STORAGE_AUTH_KEY = "authState";
+
+const AuthProvider = ({ children }) => {
+  const [state, setState] = useState(false);
+  useEffect(() => {
+    const userData =
+      JSON.parse(localStorage.getItem(LOCAL_STORAGE_AUTH_KEY)) || false;
+    setState(userData);
+  }, []);
+  useEffect(() => {
+    const data = localStorage.setItem(
+      LOCAL_STORAGE_AUTH_KEY,
+      JSON.stringify(state)
+    );
+  }, [state]);
+  return (
+    <AuthContext.Provider value={state}>
+      <AuthContextDispatcher.Provider value={setState}>
+        {children}
+      </AuthContextDispatcher.Provider>
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
+export const useAuth = () => useContext(AuthContext);
+export const useAuthActions = () => useContext(AuthContextDispatcher);
