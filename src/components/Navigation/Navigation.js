57ed39ff4cd6486b4023665abff1107c0e9bb1c1
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-11-23 13:12:07 +0330

    add profile link to navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,29 @@
 import { NavLink, withRouter } from "react-router-dom";
+import { useAuth } from "../../Context/AuthProvider";
 import { useCart } from "../../Context/CartProvider";
 import "./Navigation.css";
 
 const Navigation = () => {
   const { cart } = useCart();
+  const userData = useAuth();
   return (
     <header className="mainNavigation">
       <nav>
         <ul>
+          <div>Shopping</div>
           <li>
             <NavLink to="/" activeClassName="activeLink" exact>
               home
+            </NavLink>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <NavLink
+              to={userData ? "/profile" : "/login"}
+              activeClassName="activeLink"
+            >
+              {userData ? `${userData.name} welcome` : "login/signup"}
             </NavLink>
           </li>
           <li className="cartLink">
@@ -20,7 +33,6 @@ const Navigation = () => {
             <span> {cart.length}</span>
           </li>
         </ul>
-        <div>Shopping</div>
       </nav>
     </header>
   );
