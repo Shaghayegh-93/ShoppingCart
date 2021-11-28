e683ed2882f709389f55bbda98d36f93016a5a60
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-11-28 12:07:46 +0330

    add login page for having login root

---
+++ src/Pages/LoginPage.js
@@ -0,0 +1,13 @@
+import LoginForm from "../components/Login/LoginForm";
+
+import Layout from "../Layout/Layout";
+
+const LoginPage = () => {
+  return (
+    <Layout>
+      <LoginForm />
+    </Layout>
+  );
+};
+
+export default LoginPage;
