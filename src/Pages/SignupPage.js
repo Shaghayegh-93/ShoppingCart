e35ca08fbec7ecfa3136126647b6475328224f23
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-11-28 12:06:58 +0330

    add signup page for having signup root

---
+++ src/Pages/SignupPage.js
@@ -0,0 +1,12 @@
+import SignupForm from "../components/Signup/SignupForm";
+import Layout from "../Layout/Layout";
+
+const SignupPage = () => {
+  return (
+    <Layout>
+      <SignupForm />
+    </Layout>
+  );
+};
+
+export default SignupPage;
