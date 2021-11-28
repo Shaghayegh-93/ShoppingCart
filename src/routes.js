017ba1bdef920ef94706ddaf898e4fdff7346ff9
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-11-28 12:11:27 +0330

    add profile root

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import CartPage from "./Pages/CartPage";
 import ChekoutPage from "./Pages/ChekoutPage";
 import LoginPage from "./Pages/LoginPage";
 import SignupPage from "./Pages/SignupPage";
+import Profile from "./components/profile/Profile";
 const routes = [
   { path: "/cart", component: CartPage },
   { path: "/checkout", component: ChekoutPage },
-  { path: "./login", component: LoginPage },
-  { path: "./signup", component: SignupPage },
+  { path: "/profile", component: Profile },
+  { path: "/login", component: LoginPage },
+  { path: "/signup", component: SignupPage },
   { path: "/", component: HomePage, exact: true },
 ];
 
